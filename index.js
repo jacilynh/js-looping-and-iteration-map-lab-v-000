254d8784f5d594280eafbe54795c764d4d145451
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-22 00:18:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function lowerCaseDrivers(drivers) {
 function nameToAttributes(drivers) {
   return drivers.map(d => Object.assign({}, {firstName: d.split(" ")[0]}, {lastName: d.split(" ")[1]}))
 }
+
+function attributesToPhrase(drivers) {
+  
+}
